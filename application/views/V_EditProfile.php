bf294ef95648096cbe70ddb440bcdd4891147b50
Ferri L Halim <ferrilasmihalim@gmail.com>
2019-08-27 14:49:58 +0700

    cleaning + a lil bit fixing

--- application/views/V_EditProfile.php
+++ application/views/V_EditProfile.php
@@ -6,126 +6,107 @@
   ?>
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-        Page <?php echo $title;?>
-        <small>Optional description</small>
-      </h1>
-      <ol class="breadcrumb">
-          <li><a href="<?php echo base_url('admin')?>"><i class="fa fa-home"></i></a></li>
-          <li class="active"><?php echo $title?></li>
-      </ol>
-    </section>
-
+	  <section class="content-header">
+		  <h1>
+			  Page <?php echo $title;?>
+			  <small>Optional description</small>
+		  </h1>
+		  <ol class="breadcrumb">
+			  <li><a href="<?php if ($this->session->userdata('isLogin') == "partner"){ echo base_url('home');}else{echo base_url('admin');}?>"><i class="fa fa-home"></i></a></li>
+			  <li class="active"><?php echo $title?></li>
+		  </ol>
+	  </section>
     <!-- Main content -->
-    <section class="content container-fluid">
+	  <section class="content container-fluid">
+		  <div class="row">
+			  <div class="col-md-3">
+				  <!-- Profile Image -->
+				  <div class="box box-primary">
+					  <div class="box-body box-profile">
+						  <img class="profile-user-img img-responsive " src="<?php echo base_url()?>assets/images/user.png"alt="User profile picture">
+						  <h3 class="profile-username text-center"></h3>
 
-      <div class="row">
-            <div class="col-md-3">
+						  <p class="text-muted text-center"></p>
 
-                <!-- Profile Image -->
-                <div class="box box-primary">
-                    <div class="box-body box-profile">
-                        <img class="profile-user-img img-responsive " src="<?php echo base_url()?>assets/dist/img/user2-160x160.jpg"alt="User profile picture">
-                        <h3 class="profile-username text-center"></h3>
+						  <ul class="list-group list-group-unbordered">
+							  <li class="list-group-item">
+								  <b>Nama</b><a class="pull-right"><?php echo $this->session->userdata('nama') ?></a>
+							  </li>
+							  <li class="list-group-item">
+								  <b>Email</b> <a class="pull-right"><?php echo $this->session->userdata('email') ?></a>
+							  </li>
+						  </ul>
+					  </div>
+				  </div>
+			  </div>
+			  <div class="col-md-9">
+				  <div class="box box-primary">
+					  <div class="box-header with-border">
+						  <h3 class="box-title">Edit Profile</h3>
+					  </div>
+					  <!-- /.box-header -->
+					  <!-- form start -->
+					  <form class="form-horizontal" action="<?php echo base_url('C_Profile/updateProfile/'.$id)?>" method="post">
+						  <div class="box-body">
+							  <?php if(validation_errors() || $this->session->flashdata('failed')){ ?>
+								  <div class="alert alert-warning">
+									  <button type="button" class="close" data-dismiss="alert">&times;</button>
+									  <strong>Warning</strong>
+									  <?php echo validation_errors(); ?>
+									  <?php echo $this->session->flashdata('failed'); ?>
+								  </div>
+							  <?php } else {?>
+								  <?php if (validation_errors() || $this->session->flashdata('success'))
+								  {?>
+									  <div class="alert alert-success">
+										  <button type="button" class="close" data-dismiss="alert">&times;</button>
+										  <strong>Success</strong>
+										  <?php echo validation_errors(); ?>
+										  <?php echo $this->session->flashdata('success'); ?>
+									  </div>
+								  <?php } } ?>
+							  <div class="form-group">
+								  <label for="nama" class="col-sm-2 control-label">Nama</label>
 
-                        <p class="text-muted text-center"></p>
+								  <div class="col-sm-10">
+									  <?php if ($this->session->userdata('isLogin') == "partner"){ ?>
+										  <input type="text" class="form-control" id="nama" placeholder="Nama" name="nama" value="<?php echo $this->session->userdata('nama')?>" disabled>
+									  <?php } else{?>
+										  <input type="text" class="form-control" id="nama" placeholder="Nama" name="nama" >
+									  <?php }?>
+								  </div>
+							  </div>
+							  <div class="form-group">
+								  <label for="email" class="col-sm-2 control-label">Email</label>
 
-                        <ul class="list-group list-group-unbordered">
-                            <li class="list-group-item">
-                                <b>Nama</b><a class="pull-right"><?php echo $this->session->userdata('nama') ?></a>
-                            </li>
-                            <li class="list-group-item">
-                                <b>Email</b> <a class="pull-right"><?php echo $this->session->userdata('email') ?></a>
-                            </li>
-                        </ul>
-                    </div>
-                    <!-- /.box-body -->
-                </div>
-                <!-- /.box -->
+								  <div class="col-sm-10">
+									  <input type="email" class="form-control" id="email" placeholder="Email" name="email">
+								  </div>
+							  </div>
+							  <div class="form-group">
+								  <label for="password" class="col-sm-2 control-label">Password</label>
 
-                <!-- About Me Box -->
+								  <div class="col-sm-10">
+									  <input type="password" class="form-control" id="password" placeholder="Password" name="password">
+								  </div>
+							  </div>
+							  <div class="form-group">
+								  <label for="re-password" class="col-sm-2 control-label">Confirm Password</label>
 
-                <!-- /.box -->
-            </div>
-            <!-- /.col -->
-            <div class="col-md-9">
-                <div class="nav-tabs-custom">
-                    <div class="tab-content">
-						<div class="box box-info">
-							<div class="box-header with-border">
-								<h3 class="box-title">Edit Profile</h3>
-							</div>
-							<!-- /.box-header -->
-							<!-- form start -->
-							<form class="form-horizontal" action="<?php echo base_url('C_Profile/updateProfile/'.$id)?>" method="post">
-								<div class="box-body">
-									<?php if(validation_errors() || $this->session->flashdata('failed')){ ?>
-										<div class="alert alert-warning">
-											<button type="button" class="close" data-dismiss="alert">&times;</button>
-											<strong>Warning</strong>
-											<?php echo validation_errors(); ?>
-											<?php echo $this->session->flashdata('failed'); ?>
-										</div>
-									<?php } else {?>
-									<?php if (validation_errors() || $this->session->flashdata('success'))
-										{?>
-											<div class="alert alert-success">
-												<button type="button" class="close" data-dismiss="alert">&times;</button>
-												<strong>Success</strong>
-												<?php echo validation_errors(); ?>
-												<?php echo $this->session->flashdata('success'); ?>
-											</div>
-										<?php } } ?>
-									<div class="form-group">
-										<label for="nama" class="col-sm-2 control-label">Nama</label>
-
-										<div class="col-sm-10">
-											<?php if ($this->session->userdata('isLogin') == "partner"){ ?>
-											<input type="text" class="form-control" id="nama" placeholder="Nama" name="nama" value="<?php echo $this->session->userdata('nama')?>" disabled>
-											<?php } else{?>
-											<input type="text" class="form-control" id="nama" placeholder="Nama" name="nama" >
-											<?php }?>
-										</div>
-									</div>
-									<div class="form-group">
-										<label for="email" class="col-sm-2 control-label">Email</label>
-
-										<div class="col-sm-10">
-											<input type="email" class="form-control" id="email" placeholder="Email" name="email">
-										</div>
-									</div>
-									<div class="form-group">
-										<label for="password" class="col-sm-2 control-label">Password</label>
-
-										<div class="col-sm-10">
-											<input type="password" class="form-control" id="password" placeholder="Password" name="password">
-										</div>
-									</div>
-									<div class="form-group">
-										<label for="re-password" class="col-sm-2 control-label">Confirm Password</label>
-
-										<div class="col-sm-10">
-											<input type="password" class="form-control" id="re-password" placeholder="Confirm Password" name="re-password">
-										</div>
-									</div>
-								</div>
-								<!-- /.box-body -->
-								<div class="box-footer">
-									<button type="submit" class="btn btn-info pull-right">Confirm</button>
-								</div>
-								<!-- /.box-footer -->
-							</form>
-						</div>
-
-						<!-- /.tab-content -->
-                </div>
-                <!-- /.nav-tabs-custom -->
-            </div>
-            <!-- /.col -->
-        </div>
-
-    </section>
+								  <div class="col-sm-10">
+									  <input type="password" class="form-control" id="re-password" placeholder="Confirm Password" name="re-password">
+								  </div>
+							  </div>
+						  </div>
+						  <!-- /.box-body -->
+						  <div class="box-footer">
+							  <button type="submit" class="btn btn-primary pull-right">Confirm</button>
+						  </div>
+						  <!-- /.box-footer -->
+					  </form>
+				  </div>
+			  </div>
+	  </section>
     <!-- /.content -->
   </div>
 
--- application/views/parts/V_Navigation.php
+++ application/views/parts/V_Navigation.php
@@ -1,24 +1,3 @@
-
-<!--
-BODY TAG OPTIONS:
-=================
-Apply one or more of the following classes to get the
-desired effect
-|---------------------------------------------------------|
-| SKINS         | skin-blue                               |
-|               | skin-black                              |
-|               | skin-purple                             |
-|               | skin-yellow                             |
-|               | skin-red                                |
-|               | skin-green                              |
-|---------------------------------------------------------|
-|LAYOUT OPTIONS | fixed                                   |
-|               | layout-boxed                            |
-|               | layout-top-nav                          |
-|               | sidebar-collapse                        |
-|               | sidebar-mini                            |
-|---------------------------------------------------------|
--->
 <body class="hold-transition skin-blue sidebar-mini">
   <?php
 $name 	= $this->session->userdata('nama');
@@ -49,115 +28,19 @@ $id 	= $this->session->userdata('id_user');
       <!-- Navbar Right Menu -->
       <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
-          <!-- Messages: style can be found in dropdown.less-->
-          <li class="dropdown messages-menu">
-            <!-- Menu toggle button -->
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <i class="fa fa-envelope-o"></i>
-              <span class="label label-success">4</span>
-            </a>
-            <ul class="dropdown-menu">
-              <li class="header">You have 4 messages</li>
-              <li>
-                <!-- inner menu: contains the messages -->
-                <ul class="menu">
-                  <li><!-- start message -->
-                    <a href="#">
-                      <div class="pull-left">
-                        <!-- User Image -->
-                        <img src="<?php echo base_url()?>assets/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-                      </div>
-                      <!-- Message title and timestamp -->
-                      <h4>
-                        Support Team
-                        <small><i class="fa fa-clock-o"></i> 5 mins</small>
-                      </h4>
-                      <!-- The message -->
-                      <p>Why not buy a new awesome theme?</p>
-                    </a>
-                  </li>
-                  <!-- end message -->
-                </ul>
-                <!-- /.menu -->
-              </li>
-              <li class="footer"><a href="#">See All Messages</a></li>
-            </ul>
-          </li>
-          <!-- /.messages-menu -->
-
-          <!-- Notifications Menu -->
-          <li class="dropdown notifications-menu">
-            <!-- Menu toggle button -->
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <i class="fa fa-bell-o"></i>
-              <span class="label label-warning">10</span>
-            </a>
-            <ul class="dropdown-menu">
-              <li class="header">You have 10 notifications</li>
-              <li>
-                <!-- Inner Menu: contains the notifications -->
-                <ul class="menu">
-                  <li><!-- start notification -->
-                    <a href="#">
-                      <i class="fa fa-users text-aqua"></i> 5 new members joined today
-                    </a>
-                  </li>
-                  <!-- end notification -->
-                </ul>
-              </li>
-              <li class="footer"><a href="#">View all</a></li>
-            </ul>
-          </li>
-          <!-- Tasks Menu -->
-          <li class="dropdown tasks-menu">
-            <!-- Menu Toggle Button -->
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <i class="fa fa-flag-o"></i>
-              <span class="label label-danger">9</span>
-            </a>
-            <ul class="dropdown-menu">
-              <li class="header">You have 9 tasks</li>
-              <li>
-                <!-- Inner menu: contains the tasks -->
-                <ul class="menu">
-                  <li><!-- Task item -->
-                    <a href="#">
-                      <!-- Task title and progress text -->
-                      <h3>
-                        Design some buttons
-                        <small class="pull-right">20%</small>
-                      </h3>
-                      <!-- The progress bar -->
-                      <div class="progress xs">
-                        <!-- Change the css width attribute to simulate progress -->
-                        <div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar"
-                             aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                          <span class="sr-only">20% Complete</span>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
-                  <!-- end task item -->
-                </ul>
-              </li>
-              <li class="footer">
-                <a href="#">View all tasks</a>
-              </li>
-            </ul>
-          </li>
           <!-- User Account Menu -->
           <li class="dropdown user user-menu">
             <!-- Menu Toggle Button -->
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <!-- The user image in the navbar-->
-              <img src="<?php echo base_url()?>assets/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+              <img src="<?php echo base_url()?>assets/images/user.png" class="user-image" alt="User Image">
               <!-- hidden-xs hides the username on small devices so only the image appears. -->
               <span class="hidden-xs"><?php echo $name;?></span>
             </a>
             <ul class="dropdown-menu">
               <!-- The user image in the menu -->
               <li class="user-header">
-                <img src="<?php echo base_url()?>assets/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                <img src="<?php echo base_url()?>assets/images/user.png" class="img-circle" alt="User Image">
 
                 <p>
                   <?php echo $name;?>
@@ -168,10 +51,10 @@ $id 	= $this->session->userdata('id_user');
               <!-- Menu Footer-->
               <li class="user-footer">
                 <div class="pull-left">
-                  <a href="<?php echo base_url('profile/'.$id)?>" class="btn btn-default btn-flat">Profile</a>
+                  <a href="<?php echo base_url('profile/'.$id)?>" class="btn btn-primary btn-flat">Profile</a>
                 </div>
                 <div class="pull-right">
-                  <a href="<?php if ($login=='admin'){echo base_url('logoutAdmin');}else{echo base_url('logoutPartner');}?>" class="btn btn-default btn-flat">Sign out</a>
+                  <a href="<?php if ($login=='admin'){echo base_url('logoutAdmin');}else{echo base_url('logoutPartner');}?>" class="btn btn-danger btn-flat">Sign out</a>
                 </div>
               </li>
             </ul>
@@ -189,7 +72,7 @@ $id 	= $this->session->userdata('id_user');
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="<?php echo base_url()?>assets/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+          <img src="<?php echo base_url()?>assets/images/user.png" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
           <p><?php echo $name;?></p>
@@ -198,21 +81,9 @@ $id 	= $this->session->userdata('id_user');
         </div>
       </div>
 
-      <!-- search form (Optional) -->
-      <form action="#" method="get" class="sidebar-form">
-        <div class="input-group">
-          <input type="text" name="q" class="form-control" placeholder="Search...">
-          <span class="input-group-btn">
-              <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-              </button>
-            </span>
-        </div>
-      </form>
-      <!-- /.search form -->
-
       <!-- Sidebar Menu -->
       <ul class="sidebar-menu" data-widget="tree">
-        <li class="header">Navigation <?php echo $title;?></li>
+        <li class="header">Navigation Menu</li>
         <!-- Optionally, you can add icons to the links -->
 		  <?php if ($login=='admin') {
 		  	?>
